03daf96dbf754b90b58533d0cb6ff172492679ef
chenshuai <1183942503@qq.com>
2024-09-06 17:51:37 +0800

    [style]: 切割字体包，提供加载速度

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import Layout from './layout.vue'
+import './font/font.css'
 import './custom.css'
 
 export default {
---
+++ docs/public/cc.js
@@ -0,0 +1,10 @@
+// 切割字体包
+// const createFontSlice = require('font-slice');
+// const path = require('path');
+
+// createFontSlice({
+//   // fontPath
+//   fontPath: path.resolve(__dirname, 'HYWenHei-85W.ttf'),
+//   // outputDir
+//   outputDir: path.resolve(__dirname, './font'),
+// })
